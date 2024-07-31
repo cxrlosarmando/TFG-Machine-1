5ffc8504e83275d4f7928543c6e11c571ce9cf96
Diego Contreras <66972191+LilDie@users.noreply.github.com>
2024-07-31 11:01:14 -0400

    pruebas con s.io y cambios rutas din

--- src/app/api/juegosApi/route.ts
+++ src/app/api/juegosApi/route.ts
@@ -2,65 +2,63 @@ import GamesModel from '@/models/games';
 import { NextResponse } from 'next/server';
 import { connectDB } from '@/libs/mongodb';
 
-//GET ALL MACHINE
-export async function GET() {
-    try {
-      // Conectar a la base de datos
-      await connectDB();
-  
-      // Obtener todos los clientes con los datos del usuario asociado poblados
-      const juegos = await GamesModel.find();
-  
-      // Devolver la respuesta con los datos de los clientes
-      return NextResponse.json({
-        message: "Ok",
-        data: juegos,
-      }, {
-        status: 200
-      });
-    } catch (error) {
-      // Manejar cualquier error que pueda ocurrir durante el proceso
-      return NextResponse.json({
-        message: "Failed to get juegos",
-        error,
-      }, {
-        status: 500,
-      });
-    }
+// Definir la respuesta dinámica para la solicitud GET
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: Request) {
+  try {
+    // Conectar a la base de datos
+    await connectDB();
+
+    // Obtener todos los juegos
+    const juegos = await GamesModel.find();
+
+    // Devolver la respuesta con los datos de los juegos
+    return NextResponse.json({
+      message: "Ok",
+      data: juegos,
+    }, {
+      status: 200
+    });
+  } catch (error) {
+    // Manejar cualquier error que pueda ocurrir durante el proceso
+    return NextResponse.json({
+      message: "Failed to get juegos",
+      error,
+    }, {
+      status: 500,
+    });
   }
+}
 
+// Definir la respuesta dinámica para la solicitud POST
+export async function POST(request: Request) {
+  try {
+    const { provider_name, provider, games, status, img } = await request.json();
 
+    // Conectar a la base de datos
+    await connectDB();
 
-// //CREAR PROVEEDORES
-// export async function POST(request: { json: () => PromiseLike<{ provider_name: string; provider: number; games: string[]; status: boolean; img: string; }> }) {
-//   try {
-//     const { provider_name, provider, games, status, img } = await request.json();
+    // Crear un nuevo proveedor de juegos
+    const newGameProvider = await GamesModel.create({
+      provider_name: provider_name,
+      provider: provider,
+      games: games,
+      status: status,
+      img: img,
+    });
 
-//     // Conecta a la base de datos
-//     await connectDB();
-
-//     // Crea un nuevo proveedor de juegos en base a los datos proporcionados
-//     const newGameProvider = await GamesModel.create({
-//       provider_name: provider_name,
-//       provider: provider,
-//       games: games,
-//       status: status,
-//       img: img,
- 
-//     });
-
-//     return NextResponse.json({
-//       message: "Proveedor de Juegos Creado con Éxito",
-//       data: newGameProvider
-//     }, { status: 201 });
-//   } catch (error) {
-//     console.error("Error al crear el proveedor de juegos:", error);
-//     return NextResponse.json({
-//       message: "Error al crear el proveedor de juegos",
-//       error,
-//     }, {
-//       status: 500,
-//     });
-//   }
-// }
-
+    return NextResponse.json({
+      message: "Proveedor de Juegos Creado con Éxito",
+      data: newGameProvider
+    }, { status: 201 });
+  } catch (error) {
+    console.error("Error al crear el proveedor de juegos:", error);
+    return NextResponse.json({
+      message: "Error al crear el proveedor de juegos",
+      error,
+    }, {
+      status: 500,
+    });
+  }
+}
